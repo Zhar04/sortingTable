e51a9cf30b82d8ccc0c017b2b15dedd85fb7a7a5
Zhar04 <72005018+Zhar04@users.noreply.github.com>
2022-05-28 21:20:03 +0600

    a

---
+++ build/precache-manifest.38fdf4263420e7985bde1623f7235d15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c55e73b095fc38c4ea5413359c496e5",
+    "url": "/sortingTable/index.html"
+  },
+  {
+    "revision": "3d0c28e031fc7db5d2b4",
+    "url": "/sortingTable/static/css/main.0086175d.chunk.css"
+  },
+  {
+    "revision": "625d8ca221cb789bb3a9",
+    "url": "/sortingTable/static/js/2.497f34ea.chunk.js"
+  },
+  {
+    "revision": "3d0c28e031fc7db5d2b4",
+    "url": "/sortingTable/static/js/main.c3e4d356.chunk.js"
+  },
+  {
+    "revision": "f6a0932e26dc9fa24cfc",
+    "url": "/sortingTable/static/js/runtime~main.e370da4c.js"
+  }
+]);
